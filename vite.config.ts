ab8248c3ee397e15de5e3d07e40e3c915ef78b1c
alxdr3k <alxdr3k@gmail.com>
2025-06-15 22:31:21 +0900

    add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ export default defineConfig({
 	server: {
 		host: '0.0.0.0',
 		port: 5173,
-		allowedHosts: [
-			'sujin-yungeun.mooo.com'
-		]
+		allowedHosts: true
 	},
 	css: {
 		preprocessorOptions: {
