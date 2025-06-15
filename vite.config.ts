99852983fe19e7d61426cc6a80730c1672adc177
alxdr3k <alxdr3k@gmail.com>
2025-06-15 21:48:12 +0900

    add allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: 80,
+		allowedHosts: [
+			'sujin-yungeun.mooo.com'
+		]
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
