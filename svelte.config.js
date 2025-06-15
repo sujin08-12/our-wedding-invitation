22a059dfde3e38d5e8a65ef31a5a077b11444b59
alxdr3k <alxdr3k@gmail.com>
2025-06-15 17:25:08 +0900

    replace cloudflare with netflify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			// If you have a lot of routes to prerender, you may want to enable this for better performance
+			// See: https://kit.svelte.dev/docs/adapter-netlify#pre-rendering-and-serverless-functions
+			entries: ['*']
+		},
+		output: {
+			preloadStrategy: 'base'
+		}
 	}
 };
 
